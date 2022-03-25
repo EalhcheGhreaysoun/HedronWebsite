d7c47e3649a54ddde1438ece5ecb1665eda93edc
EalhcheGhreaysoun <jensencyl@gmail.com>
2022-03-25 07:30:29 -0700

    0.0.9

---
+++ scripts.js
@@ -0,0 +1,61 @@
+var isDark = true;
+
+document.getElementsByClass("colorThemeButton").onclick = function() {whiteColorSceme()};
+
+function change(){
+    if (isDark = true){
+        whiteColorSceme();
+    }
+    else if (isDark = false){
+        blackColorSceme();
+    }
+}
+
+function whiteColorSceme(){
+    var dropdownContent = document.getElementsByClass('dropdown-content');
+    var logoTitle = document.getElementsByClass('logoTitle');
+
+    var pageTitle = document.getElementById('pageTitle');
+
+    var bodyElements = document.getElementsByTag('body');
+    var links = document.getElementsByTag('a');
+
+    var switchTheme = document.getElementById('colorThemeButton');
+
+    bodyElements.style.backgroundColor = white;
+    bodyElements.style.color = black;
+
+    dropdownContent.style.backgroundColor = black;
+
+    links.style.color = black;
+
+    logoTitle.style.color = black;
+
+    switchTheme.style.backgroundImage = "url(src/moon.png)";
+    isDark = false;
+
+};
+function blackColorSceme()
+{
+    var dropdownContent = document.getElementsByClass('dropdown-content');
+    var logoTitle = document.getElementsByClass('logoTitle');
+
+    var pageTitle = document.getElementById('pageTitle');
+
+    var bodyElements = document.getElementsByTag('body');
+    var links = document.getElementsByTag('a');
+
+    var switchTheme = document.getElementById('colorThemeButton');
+
+    bodyElements.style.backgroundColor = black;
+    bodyElements.style.color = white;
+
+    dropdownContent.style.backgroundColor = white;
+
+    links.style.color = white;
+
+    logoTitle.style.color = white;
+
+    switchTheme.style.backgroundImage = "url(src/sun.png)";
+    isDark = true;
+};
