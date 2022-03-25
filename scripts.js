9d46396c61e6ded667b16b3a9d723c50b4655140
EalhcheGhreaysoun <jensencyl@gmail.com>
2022-03-25 09:15:35 -0700

    0.1.0

--- scripts.js
+++ scripts.js
@@ -3,33 +3,35 @@ var isDark = true;
 document.getElementsByClass("colorThemeButton").onclick = function() {whiteColorSceme()};
 
 function change(){
-    if (isDark = true){
+    if (isDark == true){
         whiteColorSceme();
     }
-    else if (isDark = false){
+    else if (isDark ==  false){
         blackColorSceme();
     }
 }
 
 function whiteColorSceme(){
     var dropdownContent = document.getElementsByClass('dropdown-content');
+    
     var logoTitle = document.getElementsByClass('logoTitle');
-
+    
     var pageTitle = document.getElementById('pageTitle');
 
     var bodyElements = document.getElementsByTag('body');
     var links = document.getElementsByTag('a');
 
     var switchTheme = document.getElementById('colorThemeButton');
+    
+    bodyElements[0].style.backgroundColor = white;
+    bodyElements[0].style.color = black;
 
-    bodyElements.style.backgroundColor = white;
-    bodyElements.style.color = black;
-
-    dropdownContent.style.backgroundColor = black;
-
-    links.style.color = black;
-
-    logoTitle.style.color = black;
+    dropdownContent[0].style.backgroundColor = black;
+    for(i=0; i < links.length; i++){
+        links[i].style.color = black;
+    }
+    
+    logoTitle[0].style.color = black;
 
     switchTheme.style.backgroundImage = "url(src/moon.png)";
     isDark = false;
